4f98d309988dc2e8e5fa5baa9d8c3ed674691734
Sanjay Karki <sanjaykarki@live.com>
2022-07-31 06:08:22 +0200

    created time dealy to move the mouse

--- beespam.py
+++ beespam.py
@@ -1,8 +1,11 @@
 import pyautogui as gui
-
-gui.click(1522, 936) # X and Y cordinate of screen
+import time
 
 f = open('beescript.txt')
+
+# this will create delay us
+# so we have time to move curser away from terminal
+time.sleep(4)
 
 for word in f.read().split():
     gui.typewrite(word, interval= 0.07) # Change interval for speed of typing
